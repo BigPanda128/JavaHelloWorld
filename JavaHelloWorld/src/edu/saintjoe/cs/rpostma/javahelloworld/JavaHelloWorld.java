4d2c2ac694333fb6525acf4f9957b9e9f8c0b38d
rpostma <rpostma@localhost>
2016-02-08 19:58:42 -0500

    My first commit to make sure I am talking to you.

---
+++ JavaHelloWorld/src/edu/saintjoe/cs/rpostma/javahelloworld/JavaHelloWorld.java
@@ -0,0 +1,10 @@
+package edu.saintjoe.cs.rpostma.javahelloworld;
+
+public class JavaHelloWorld {
+
+	public static void main(String[] args) {
+		// Hello World
+		System.out.println("Hello there, I am Ryan Postma");
+	}
+
+}
